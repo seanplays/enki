7ff612de6adbabc7f6ba36cf5d56fc0c2411291c
Sean O'Donnell <sean.reads@gmail.com>
2012-05-28 13:10:12 -0400

    set config.assets.initialize_on_precompile to false per Heroku requirement

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,7 @@ module Enki
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false # Heroku requires this to be false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
